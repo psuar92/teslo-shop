66c446d5b7c6bf439b180fb3ea1e861a021db291
Pedro <psuar92@gmail.com>
2024-11-28 12:33:29 -0500

    setup routes

---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,11 @@
+import { titleFont } from "../../config/fonts";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>Hello World</h1>
+
+      <h1 className={titleFont.className}>Hola Mundo</h1>
+    </main>
+  );
+}
