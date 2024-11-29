96af8d9fa03ca23aed6a3c612a4fc79462e7ba37
Pedro <psuar92@gmail.com>
2024-11-29 16:11:29 -0500

    update shop layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,10 +1,13 @@
 
 export default function ShopLayout({ children }: {
- children: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
-    <main className="bg-gray-500 min-h-screen">
-      {children}
+    <main className="flex justify-center">
+      <div className="w-full sm:w-[350px] px-10">
+        {children}
+      </div>
+
     </main>
   );
 }
